248264e861b1b6723177d5982366306e67f820cc
Maksim <lvutinwork@gmail.com>
2025-04-13 10:57:19 +0300

    build: create layout website and his folder structure

---
+++ src/services/MarvelService.js
@@ -0,0 +1,14 @@
+
+
+class MarvelService {
+    getResource = async (url) => {
+        const res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+    
+        return await res.json();
+    }
+}
