57cef1e7025304a7e933bfc037a1a4d48b75eda5
Maksim <lvutinwork@gmail.com>
2025-04-18 11:56:18 +0300

    feat: added function for active char on CharList component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -57,8 +57,21 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setCharRef = elem => {
+        this.itemRefs.push(elem);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected')
+        this.itemRefs[id].focus();
+    }
+
+
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'}
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'contain'}
@@ -67,9 +80,20 @@ class CharList extends Component {
 
             return (
                 <li 
-                className="char__item" 
+                ref={this.setCharRef}
+                className="char__item"
+                tabIndex={0} 
                 key={item.id}
-                onClick={() => this.props.onCharSelected(item.id)}>
+                onClick={() => {
+                    this.props.onCharSelected(item.id);
+                    this.focusOnItem(i);
+                }}
+                onKeyPress={(e) => {
+                    if (e.key === ' ' || e.key === 'Enter') {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }
+                }}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li> 
