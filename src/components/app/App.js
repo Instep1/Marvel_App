eb7e293381d4b14ef812527f8a2208fd8fc87eb1
Maksim <lvutinwork@gmail.com>
2025-04-21 08:44:36 +0300

    feat: created logic for component ComicsList

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ComicsList from '../comicsList/ComicsList';
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
@@ -20,7 +21,7 @@ const App = () => {
         <div className="app">
             <AppHeader/>
             <main>
-                <ErrorBoundary>
+                {/* <ErrorBoundary>
                     <RandomChar/>
                 </ErrorBoundary>
                 <div className="char__content">
@@ -30,7 +31,10 @@ const App = () => {
                     <ErrorBoundary>
                         <CharInfo charId={selectedChar}/>
                     </ErrorBoundary>
-                </div>
+                </div> */}
+                <ErrorBoundary>
+                    <ComicsList/>
+                </ErrorBoundary>
                 <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
         </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -19,6 +19,11 @@ const useMarvelService = () => {
         return _transformCharacter(res.data.results[0]);
     }
 
+    const getAllComics = async (offset = _baseOffset) => {
+        const res = await request(`${_apiBase}comics?limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics);
+    }
+
     const _transformCharacter = (char) => {
         return {
             id: char.id,
@@ -31,7 +36,16 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter, clearError};
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+            price: comics.prices[0].price
+        }
+    }
+
+    return {loading, error, getAllCharacters, getCharacter, getAllComics, clearError};
 }
 
 export default useMarvelService;
